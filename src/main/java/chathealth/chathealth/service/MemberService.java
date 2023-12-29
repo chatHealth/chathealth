b89c63f93a491e8048e435d9d4addb3120b76e1e
BaeChanyong <nabi3315@gmail.com>
2023-12-29 09:26:17 +0900

    auth controller ì 생성

--- src/main/java/chathealth/chathealth/service/MemberService.java
+++ src/main/java/chathealth/chathealth/service/MemberService.java
@@ -1,4 +1,10 @@
 package chathealth.chathealth.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
 }
