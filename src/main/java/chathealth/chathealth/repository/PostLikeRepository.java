87ef2fb2b4ff9ff21c3dc306b274a087143610a1
gyuusun <moni1227a@gmail.com>
2024-01-24 16:57:43 +0900

    fetch: postlike 기능생성

--- src/main/java/chathealth/chathealth/repository/PostLikeRepository.java
+++ src/main/java/chathealth/chathealth/repository/PostLikeRepository.java
@@ -1,12 +1,16 @@
 package chathealth.chathealth.repository;
 
+import chathealth.chathealth.entity.post.Post;
 import chathealth.chathealth.entity.post.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
     List<PostLike> findByMemberId(Long memberId);
-
+    Long countByPostId(Long postId);
+    PostLike findByMemberIdAndPostId(Long memberId,Long postId);
+    void deleteByMemberIdAndPostId(Long memberId, Long postId);
 
 }
