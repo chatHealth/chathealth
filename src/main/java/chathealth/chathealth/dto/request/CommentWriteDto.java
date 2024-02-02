759fff8a0767bc62f4f20f59a602aa59bba27b47
gyuusun <moni1227a@gmail.com>
2024-02-02 10:37:55 +0900

    fix: 리뷰 관련 세부 기능수정

--- src/main/java/chathealth/chathealth/dto/request/CommentWriteDto.java
+++ src/main/java/chathealth/chathealth/dto/request/CommentWriteDto.java
@@ -4,6 +4,7 @@ package chathealth.chathealth.dto.request;
 import chathealth.chathealth.entity.Review;
 import chathealth.chathealth.entity.member.Member;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -20,6 +21,7 @@ import static jakarta.persistence.GenerationType.SEQUENCE;
 public class CommentWriteDto {
     private Review review;
     private Member member;
+    @Size(max=200, message = "내용은 200자 이내로 작성해주세요.")
     private String content;
     private LocalDateTime CreatedDate;
 }
