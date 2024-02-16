d5ddd8f144f6e8e2e77a32ea30e8deec84ee8452
WowJo <dst04157@gmail.com>
2024-02-16 23:40:13 +0900

    fix: merge1.0

--- src/main/java/chathealth/chathealth/repository/boardHit/BoardHitRepository.java
+++ src/main/java/chathealth/chathealth/repository/boardHit/BoardHitRepository.java
@@ -1,4 +1,4 @@
-package chathealth.chathealth.repository.BoardHit;
+package chathealth.chathealth.repository.boardHit;
 
 import chathealth.chathealth.entity.BoardHit;
 import org.springframework.data.jpa.repository.JpaRepository;
